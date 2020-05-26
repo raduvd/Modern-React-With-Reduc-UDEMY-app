c09ac4dfe0bb740b88c9a65d776cda51dd44f582
Radu Vancea <Radu.Vancea@msg.group>
2020-05-26 15:19:50 +0300

    d

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class Header extends React.Component {
   render = () => {
     return (
       <div className="ui secondary menu">
-        <div className="right menu"></div>
+        <Link to="/" className="item">
+          Stream App
+        </Link>
+        <div className="right menu">
+          <Link to="/" className="item">
+            All Streams
+          </Link>
+        </div>
       </div>
     );
   };
