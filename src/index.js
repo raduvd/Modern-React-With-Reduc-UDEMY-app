b4d12b9b82afae0373dcdd4c28c80d4d47779e1a
Radu Vancea <Radu.Vancea@msg.group>
2020-06-11 10:24:49 +0300

    basic

--- src/index.js
+++ src/index.js
@@ -1,40 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Seasons from './Seasons';
-import Spinner from './Spinner';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-
-class App extends React.Component {
-
-    constructor() {
-        super();
-        this.state = { geolocation: null, errorMessage: null }
-    }
-
-    componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => { this.setState({ geolocation: position.coords.latitude }) },
-            (error) => { this.setState({ errorMessage: error.message }) })
-    }
-
-    renderContents() {
-        if (this.state.errorMessage && !this.state.geolocation) {
-            return <div> Error: {this.state.errorMessage}</div >;
-        }
-        if (!this.state.errorMessage && this.state.geolocation) {
-            return <Seasons geolocation={this.state.geolocation} />;
-        }
-        return <Spinner />;
-    }
-
-    render() {
-        return (
-            <div>
-                {this.renderContents()}
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(<App />, document.querySelector("#root"))
-
+ReactDOM.render(<App />, document.querySelector("#root"));
