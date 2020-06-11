9a9f797831ce3fe70f10fae3ce79eb198109d283
Radu Vancea <Radu.Vancea@msg.group>
2020-06-11 17:02:21 +0300

    mes

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,7 @@
-import React, { useState } from "react";
-import ResourceList from "./ResourceList";
-import UserList from "./UserList";
+import React from "react";
 
 const App = () => {
-  const [resource, setResource] = useState("posts");
-
-  return (
-    <div>
-      <UserList />
-      <div>
-        <button
-          onClick={() => {
-            setResource("posts");
-          }}
-        >
-          Posts
-        </button>
-        <button
-          onClick={() => {
-            setResource("todos");
-          }}
-        >
-          Todos
-        </button>
-      </div>
-      <ResourceList resourceProp={resource} />
-    </div>
-  );
+  return <div>App</div>;
 };
 
-/* class App extends React.Component {
-  state = { resource: "posts" };
-  render() {
-    return (
-      <div>
-        <div>
-          <button
-            onClick={() => {
-              this.setState({ resource: "posts" });
-            }}
-          >
-            Posts
-          </button>
-          <button
-            onClick={() => {
-              this.setState({ resource: "todos" });
-            }}
-          >
-            Todos
-          </button>
-        </div>
-        {this.state.resource}
-      </div>
-    );
-  }
-} */
 export default App;
